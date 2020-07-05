4341624be46e1e44168500f671b7f853cb590d4a
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2020-07-06 00:10:51 +0700

    Add option to enable cava widget interpolation

--- widget/desktop/cava.lua
+++ widget/desktop/cava.lua
@@ -9,13 +9,11 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 local posix = require("posix")
 
-local round = require("cosy.util").math.round
-
+local table = table
 local tostring = tostring
 
 -- Constants, depend on cava configuration
 local cava_max = 1000
-local cava_bars = 50
 
 local cava = {}
 cava.mt = {}
@@ -24,6 +22,8 @@ _G.cava_global_val = {}
 
 cava.defaults = {
     position = "top",
+    enable_interpolation = false,
+    bars = 50,
     size = 45,
     zero_size = 2,
     spacing = 5,
@@ -31,7 +31,7 @@ cava.defaults = {
 }
 
 -- Throws exception
-local function parse_fifo()
+local function parse_fifo(cava_bars)
     local cava_fifo = assert(posix.open("/tmp/cava", posix.O_RDONLY + posix.O_NONBLOCK), "Could not open /tmp/cava")
     -- Buffer has to be big enough to read all accumulated output asap. This will prevent slow updating cava
     -- from displaying outdated values
@@ -40,27 +40,22 @@ local function parse_fifo()
     posix.close(cava_fifo)
 
     local cava_val = {}
-    for match in cava_string:match("[%d;]+\n$"):gmatch("(%d+);") do
-        table.insert(cava_val, match)
+    local match = cava_string:match("[%d;]+\n$")
+    if match ~= nil then
+        for match in cava_string:match("[%d;]+\n$"):gmatch("(%d+);") do
+            table.insert(cava_val, match)
+        end
     end
 
     -- Assert prevents blinking. Sometimes because of simultaneous access to fifo incomplete string may be received.
     -- This way it may freeze a little sometimes, but it won't blink
-    assert(#cava_val == cava_bars, "Expected length of the table is 50. Actual result: " .. tostring(#cava_val))
+    assert(#cava_val == cava_bars, "Expected length of the table is "..tostring(cava_bars)..". Actual result: "..tostring(#cava_val))
 
     return cava_val
 end
 
-local function update_global_val()
-    local success, result = pcall(parse_fifo)
-
-    if success then
-        _G.cava_global_val = result
-    end
-end
-
 local function draw_top(cava_widget, context, cr, width, height)
-    local w = width / cava_bars - cava_widget.spacing  -- block width
+    local w = width / cava_widget.bars - cava_widget.spacing  -- block width
     local d = w + cava_widget.spacing           -- block distance
 
     cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
@@ -78,7 +73,7 @@ local function draw_top(cava_widget, context, cr, width, height)
 end
 
 local function draw_left(cava_widget, context, cr, width, height)
-    local w = height / cava_bars - cava_widget.spacing -- block width
+    local w = height / cava_widget.bars - cava_widget.spacing -- block width
     local d = w + cava_widget.spacing           -- block distance
 
     cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
@@ -96,7 +91,7 @@ local function draw_left(cava_widget, context, cr, width, height)
 end
 
 local function draw_bottom(cava_widget, context, cr, width, height)
-    local w = width / cava_bars - cava_widget.spacing  -- block width
+    local w = width / cava_widget.bars - cava_widget.spacing  -- block width
     local d = w + cava_widget.spacing           -- block distance
 
     cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
@@ -114,7 +109,7 @@ local function draw_bottom(cava_widget, context, cr, width, height)
 end
 
 local function draw_right(cava_widget, context, cr, width, height)
-    local w = height / cava_bars - cava_widget.spacing -- block width
+    local w = height / cava_widget.bars - cava_widget.spacing -- block width
     local d = w + cava_widget.spacing           -- block distance
 
     cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
@@ -131,6 +126,118 @@ local function draw_right(cava_widget, context, cr, width, height)
     cr:stroke()
 end
 
+local function draw_top_interpolated(cava_widget, context, cr, width, height)
+    local spacing = width / (cava_widget.bars + 1)
+    local prev_val = cava_widget.zero_size
+    local prev_pos = -spacing / 2
+    local prev_d_val = 0
+    local val = cava_widget.val[1] or cava_widget.zero_size
+    local pos = prev_pos + spacing
+    local next_pos = pos + spacing
+
+    cr:set_source(gears.color(beautiful.fg_normal))
+    cr:set_line_width(2)
+    cr:move_to(prev_pos, cava_widget.zero_size)
+
+    for i = 1, #cava_widget.val + 1 do
+        local next_val = cava_widget.val[i + 1] or cava_widget.zero_size
+        next_pos = next_pos + spacing
+
+        local d_val = spacing / 2 * (next_val - prev_val) / (next_pos - prev_pos)
+
+        cr:curve_to(prev_pos+spacing/2, prev_val + prev_d_val, pos-spacing/2, val - d_val, pos, val)
+
+        prev_val, prev_pos, prev_d_val = val, pos, d_val
+        val, pos = next_val, next_pos
+    end
+
+    cr:stroke()
+end
+
+local function draw_left_interpolated(cava_widget, context, cr, width, height)
+    local spacing = height / (cava_widget.bars + 1)
+    local prev_val = cava_widget.zero_size
+    local prev_pos = -spacing / 2
+    local prev_d_val = 0
+    local val = cava_widget.val[1] or cava_widget.zero_size
+    local pos = prev_pos + spacing
+    local next_pos = pos + spacing
+
+    cr:set_source(gears.color(beautiful.fg_normal))
+    cr:move_to(cava_widget.zero_size, prev_pos)
+    cr:set_line_width(2)
+
+    for i = 1, #cava_widget.val + 1 do
+        local next_val = cava_widget.val[i + 1] or cava_widget.zero_size
+        next_pos = next_pos + spacing
+
+        local d_val = spacing / 2 * (next_val - prev_val) / (next_pos - prev_pos)
+
+        cr:curve_to(prev_val + prev_d_val, prev_pos+spacing/2, val - d_val, pos-spacing/2, val, pos)
+
+        prev_val, prev_pos, prev_d_val = val, pos, d_val
+        val, pos = next_val, next_pos
+    end
+
+    cr:stroke()
+end
+
+local function draw_bottom_interpolated(cava_widget, context, cr, width, height)
+    local spacing = width / (cava_widget.bars + 1)
+    local prev_val = cava_widget.zero_size
+    local prev_pos = -spacing / 2
+    local prev_d_val = 0
+    local val = cava_widget.val[1] or cava_widget.zero_size
+    local pos = prev_pos + spacing
+    local next_pos = pos + spacing
+
+    cr:set_source(gears.color(beautiful.fg_normal))
+    cr:set_line_width(2)
+    cr:move_to(prev_pos, height - cava_widget.zero_size)
+
+    for i = 1, #cava_widget.val + 1 do
+        local next_val = cava_widget.val[i + 1] or cava_widget.zero_size
+        next_pos = next_pos + spacing
+
+        local d_val = spacing / 2 * (next_val - prev_val) / (next_pos - prev_pos)
+
+        cr:curve_to(prev_pos+spacing/2, height - prev_val - prev_d_val, pos-spacing/2, height - val + d_val, pos, height - val)
+
+        prev_val, prev_pos, prev_d_val = val, pos, d_val
+        val, pos = next_val, next_pos
+    end
+
+    cr:stroke()
+end
+
+local function draw_right_interpolated(cava_widget, context, cr, width, height)
+    local spacing = height / (cava_widget.bars + 1)
+    local prev_val = cava_widget.zero_size
+    local prev_pos = -spacing / 2
+    local prev_d_val = 0
+    local val = cava_widget.val[1] or cava_widget.zero_size
+    local pos = prev_pos + spacing
+    local next_pos = pos + spacing
+
+    cr:set_source(gears.color(beautiful.fg_normal))
+    cr:move_to(cava_widget.zero_size, prev_pos)
+    cr:set_line_width(2)
+
+    for i = 1, #cava_widget.val + 1 do
+        local next_val = cava_widget.val[i + 1] or cava_widget.zero_size
+        next_pos = next_pos + spacing
+
+        local d_val = spacing / 2 * (next_val - prev_val) / (next_pos - prev_pos)
+
+        cr:curve_to(width - prev_val - prev_d_val, prev_pos+spacing/2, width - val + d_val, pos-spacing/2, width - val, pos)
+
+        prev_val, prev_pos, prev_d_val = val, pos, d_val
+        val, pos = next_val, next_pos
+    end
+
+    cr:stroke()
+end
+
 -- TODO: Document properties
 -- update_time - how often widget is redrawn. For single widget 0.05 works best. However, if more than one widget is
 -- used, it is better to set to 0.01. Widgets sometimes interrupt each other's reading of fifo and faster redraw rate
@@ -142,25 +249,25 @@ function cava.new(s, properties)
 
     -- Assigning draw function prevents multiple string comparison in performance critical code
     if     cava_widget.position == "top" then
-        cava_widget.draw = draw_top
+        cava_widget.draw = cava_widget.enable_interpolation and draw_top_interpolated or draw_top
         properties.w = s.geometry.width
         properties.h = properties.size
         if not properties.x then properties.x = 0 end
         if not properties.y then properties.y = 0 end
     elseif cava_widget.position == "left" then
-        cava_widget.draw = draw_left
+        cava_widget.draw = cava_widget.enable_interpolation and draw_left_interpolated or draw_left
         properties.w = properties.size
         properties.h = s.geometry.height
         if not properties.x then properties.x = 0 end
         if not properties.y then properties.y = 0 end
     elseif cava_widget.position == "bottom" then
-        cava_widget.draw = draw_bottom
+        cava_widget.draw = cava_widget.enable_interpolation and draw_bottom_interpolated or draw_bottom
         properties.w = s.geometry.width
         properties.h = properties.size
         if not properties.x then properties.x = 0 end
         if not properties.y then properties.y = s.geometry.height - properties.size end
     elseif cava_widget.position == "right" then
-        cava_widget.draw = draw_right
+        cava_widget.draw = cava_widget.enable_interpolation and draw_right_interpolated or draw_right
         properties.w = properties.size
         properties.h = s.geometry.height
         if not properties.x then properties.x = s.geometry.width - properties.size end
@@ -169,24 +276,28 @@ function cava.new(s, properties)
         error("Wrong cava widget position")
     end
 
-    for i = 1, cava_bars do
+    for i = 1, cava_widget.bars do
         cava_widget.val[i] = 0
     end
 
     function cava_widget:fit(context, width, height) return width, height end
 
     function cava_widget:update_val()
-        update_global_val()
+        local success, result = pcall(parse_fifo, cava_widget.bars)
+
+        if success then
+            _G.cava_global_val = result
+        end
 
         local cava_val = _G.cava_global_val
 
-        assert(#cava_val == cava_bars, "Global cava buffer has wrong amount of values")
+        assert(#cava_val == cava_widget.bars, "Global cava buffer has wrong amount of values")
 
         -- Adjust values to fit into the desired size
         local cava_val_fit = {}
         local cava_changed = false
         for i = 1, #cava_val do
-            cava_val_fit[i] = round(cava_val[i] * (self.size - self.zero_size) / cava_max)
+            cava_val_fit[i] = cava_val[i] * (self.size - self.zero_size) / cava_max
             if self.val[i] ~= cava_val_fit[i] then
                 cava_changed = true
             end
